99dc943c60e54599a51be2830bdc32aa4cb50a60
Todd W Crone <twcrone@gmail.com>
2015-12-10 16:06:59 -0500

    Add basic table for viewing inbox messages

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+'use strict';
+
 angular.module("MailboxApp", ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise("/inbox");
-    $stateProvider.state("inbox", {
-        url: "/inbox",
-        controller: function() {
-            console.log("Inbox...");
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/inbox");
+        $stateProvider.state("inbox", {
+            url: "/inbox",
+            templateUrl: "partials/inbox.html",
+            controller: function ($scope, messageStore) {
+                $scope.messages = messageStore.getMessages();
+            }
+        });
+    })
+    .service("messageStore", function () {
+        var messages = [];
+        var size = 100;
+
+        for (var i = 0; i < size; ++i) {
+            messages.push({
+                sender: "john.smith" + i + "@gmail.com",
+                date: Date.now() - i * 2400000000,
+                id: i,
+                subject: "Regarding report #" + i,
+                body: "Hey Todd, Where's report #" + i + "?"
+            });
         }
-    });
-});
+
+        return {
+            getMessages: function() {
+                return messages;
+            }
+        }
+    })
 
