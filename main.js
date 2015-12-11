cc2401910d3fa97955a4a3fe750a41d59ebb70ea
Todd W Crone <twcrone@gmail.com>
2015-12-10 21:56:29 -0500

    Increase to 10000 items

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ angular.module("MailboxApp", ['ui.router'])
     })
     .service("messageStore", function () {
         var messages = [];
-        var size = 1000;
+        var size = 10000;
 
         for (var i = 0; i < size; ++i) {
             messages.push({
